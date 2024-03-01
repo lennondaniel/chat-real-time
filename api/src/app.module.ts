7aab24f3ec9a6c450174a860ee5c475031c94500
Lennon Daniel <lennon.v90@gmail.com>
2024-02-29 21:49:15 -0300

    add configuration typeorm

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,13 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {ConfigModule} from "@nestjs/config";
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import { ChatModule } from './chat/chat.module';
+import configuration from "./config/configuration";
+import { Chat } from "./chat/chat.entity";
 
 @Module({
   imports: [
       ConfigModule.forRoot({
-        envFilePath: '../../.env'
-      })
+        envFilePath: '../../.env',
+        load: [configuration],
+        isGlobal: true
+      }),
+      TypeOrmModule.forRootAsync({
+          imports: [ConfigModule],
+          inject: [ConfigService],
+          useFactory: async (config: ConfigService) => ({
+              type: 'mysql',
+              host: config.get('database.host'),
+              port: config.get('database.port'),
+              database: config.get('database.name'),
+              username: config.get('database.user'),
+              password: config.get('database.password'),
+              entities: [Chat],
+              synchronize: true,
+          })
+      }),
+      TypeOrmModule.forFeature([Chat]),
+      ChatModule
   ],
   controllers: [AppController],
   providers: [AppService],
